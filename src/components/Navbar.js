ee1c0e25f32f746258c45f9ebc8cce558475e7ca
Santosh Narayanan <santosh33@gmail.com>
2023-10-07 14:54:36 +0200

    made responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,29 @@ import React from 'react'
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 import "../styles/Navbar.css";
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
     return (
         <div className="navbar">
             <div className="leftSide">
                 <img src={logo} />
+                <div className="hiddenLinks">
+                    <Link to="/">Home</Link>
+                    <Link to="/">Menu</Link>
+                    <Link to="/">About</Link>
+                    <Link to="/">Contact</Link>
+                </div>
             </div>
             <div className="rightSide">
                 <Link to="/">Home</Link>
                 <Link to="/">Menu</Link>
                 <Link to="/">About</Link>
                 <Link to="/">Contact</Link>
+                <button>
+                    <ReorderIcon />
+                </button>
+
             </div>
         </div>
     )
